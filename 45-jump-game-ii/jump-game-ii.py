990a08d6ca9f105d655d53776d42362ea5f9635e
Daksh Verma | AI Engineer <126685429+DakshVerma0001@users.noreply.github.com>
2026-08-19 23:03:39 +0530

    Time: 5 ms (60.89%) | Memory: 19.9 MB (92.86%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -5,13 +5,13 @@ class Solution:
         right = 0
         n = len(nums)
 
-        while right < n-1: #we have to stop when right reaches the last element, so to continue till the second last we used this
+        while right < n-1:
             farthest = 0
-            for i in range (left, right+1): #we used right + 1 to also consider the last element of the array
+            for i in range(left, right + 1):
                 farthest = max(farthest, i + nums[i])
-            left = right + 1 #new left will be the immediate next index of right so right + 1
-            right = farthest #farthest will become the new right
-            jump += 1 #jump = jump + 1
-            
+                
+            left = right + 1
+            right = farthest
+            jump += 1
         return jump
         
